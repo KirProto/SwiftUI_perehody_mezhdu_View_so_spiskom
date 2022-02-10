ca7a08d5ca07feb9a23f9a4187e1153b30ee046a
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 11:06:26 +0500

    Initial Commit

---
+++ Yan_SwiftUI_perehody_mezhdu_View_so_spiskom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yan_SwiftUI_perehody_mezhdu_View_so_spiskom
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yan_SwiftUI_perehody_mezhdu_View_so_spiskom/Yan_SwiftUI_perehody_mezhdu_View_so_spiskomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_perehody_mezhdu_View_so_spiskomApp.swift
+//  Yan_SwiftUI_perehody_mezhdu_View_so_spiskom
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_perehody_mezhdu_View_so_spiskomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
